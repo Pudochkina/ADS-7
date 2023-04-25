66987c274a2c22e63246a8335752010346038be3
PudochkinaAnastasia <116427670+Pudochkina@users.noreply.github.com>
2023-04-25 20:30:53 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,56 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+  private:
+    struct QItem {
+        T data;
+        QItem * next;
+        QItem * prev;
+    };
+    QItem * head, * tail;
+    TPQueue::QItem * create(const T& data) {
+        QItem * item = new QItem;
+        item -> data = data;
+        item -> next = nullptr;
+        item -> prev = nullptr;
+        return item;
+    }
+ public:
+    T pop() {
+        if (head) {
+            QItem * temp = head -> next;
+            if (temp)
+                temp -> prev = nullptr;
+            T data = head -> data;
+            delete head;
+            head = temp;
+            return data;
+        } else {
+            throw std::string("Empty!");
+        }
+    }
+    void push (const T& data) {
+        QItem * temp = head;
+        QItem * item = create(data);
+        while (temp && temp -> data.prior >= data.prior)
+            temp = temp -> next;
+        if (!temp && head) { 
+            tail -> next = item;
+            tail -> next -> prev = tail;
+            tail = item;
+        } else if (!temp && !head) {
+            head = tail = item;
+        } else if (!temp -> prev) {
+            temp -> prev = item;
+            item -> next = temp;
+            head = item;
+        } else { 
+            temp -> prev -> next = item;
+            item -> prev = temp -> prev;
+            item -> next = temp;
+            temp -> prev = item;
+        }
+    }
 };
 
 struct SYM {
